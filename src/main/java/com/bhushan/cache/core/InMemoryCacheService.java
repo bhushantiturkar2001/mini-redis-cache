b58e4ad04dfc92afc3c64736fc24d9dee2f3a4fb
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2026-01-27 23:01:53 +0530

    FEATURE: IN-MEMORY CACHE IMPLEMENTATION USING HASHMAP

---
+++ src/main/java/com/bhushan/cache/core/InMemoryCacheService.java
@@ -0,0 +1,37 @@
+package com.bhushan.cache.core;
+
+import com.bhushan.cache.model.CacheEntry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryCacheService<K, V> implements CacheService<K, V> {
+
+    private final Map<K, CacheEntry<K, V>> cache = new HashMap<>();
+    
+    @Override
+    public void put(K key, V value) {
+        CacheEntry<K, V> entry = new CacheEntry<>(key, value, -1);
+        cache.put(key, entry);
+    }
+
+    @Override
+    public void put(K key, V value, long ttlMillis) {
+        CacheEntry<K, V> entry = new CacheEntry<>(key, value, ttlMillis);
+        cache.put(key, entry);
+    }
+
+    @Override
+    public V get(K key) {
+        CacheEntry<K, V> entry = cache.get(key);
+        if (entry == null) {
+            return null;
+        }
+        return entry.getValue();
+    }
+
+    @Override
+    public void delete(K key) {
+        cache.remove(key);
+    }
+}
