4bb8c11a2d1df21937d238b4fed52a30ee5673a8
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2026-03-16 21:56:03 +0530

    FEATURE: ADD BACKGROUND THREAD FOR EXPIRED ENTRY CLEANUP

--- src/main/java/com/bhushan/cache/core/InMemoryCacheService.java
+++ src/main/java/com/bhushan/cache/core/InMemoryCacheService.java
@@ -3,6 +3,7 @@ package com.bhushan.cache.core;
 import com.bhushan.cache.model.CacheEntry;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class InMemoryCacheService<K, V> implements CacheService<K, V> {
@@ -43,5 +44,35 @@ public class InMemoryCacheService<K, V> implements CacheService<K, V> {
     @Override
     public void delete(K key) {
         cache.remove(key);
+    }
+
+
+    public void cleanupExpiredEntries() {
+        Iterator<Map.Entry<K, CacheEntry<K, V>>> iterator = cache.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Map.Entry<K, CacheEntry<K, V>> mapEntry = iterator.next();
+
+            if (mapEntry.getValue().isExpired()) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public InMemoryCacheService() {
+
+        Thread cleanupThread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(5000);
+                    cleanupExpiredEntries();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+
+        cleanupThread.setDaemon(true);
+        cleanupThread.start();
     }
 }
